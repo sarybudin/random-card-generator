9751dcc7edaa427356625d52bff1e8e511316adb
sarybudin <sarybudin@gmail.com>
2021-10-23 15:19:36 +0000

    final

--- src/app.js
+++ src/app.js
@@ -8,4 +8,22 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let icon = ["♦", "♥", "♠", "♣"];
+  let iconClass = ["diamond", "heart", "spade", "club"];
+  let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  let iconR = Math.floor(Math.random() * icon.length);
+  let numR = Math.floor(Math.random() * num.length);
+
+  let divNum = document.querySelector(".card>div:nth-child(2)");
+  divNum.innerHTML = num[numR];
+
+  let head = document.querySelector(".card>div:nth-child(1)");
+  head.innerHTML = "<font>" + icon[iconR] + "</font>";
+
+  let footer = document.querySelector(".card>div:nth-child(3)");
+  footer.innerHTML = "<font>" + icon[iconR] + "</font>";
+
+  let card = document.querySelector(".card");
+  card.classList.add(iconClass[iconR]);
 };
